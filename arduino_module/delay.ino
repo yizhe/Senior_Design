9a2b7c037c74e0012ca8a4dd8106862db4e03157
yizhe <yizhe@seas.upenn.edu>
2015-04-16 17:23:02 -0400

    Added arduino code to repo

---
+++ arduino_module/delay.ino
@@ -0,0 +1,45 @@
+
+unsigned long timeBefore = 0;
+int interval = 200;
+int state[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
+int pin[] = {22, 25, 29, 33, 37, 41, 45, 49, 53};
+String str;
+int index = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(23, INPUT);
+  int i;
+  for (i = 0; i < 9; i++) {
+    pinMode(pin[i], OUTPUT);
+  }
+}
+void loop() {
+  unsigned long timeNow = millis();
+  if (Serial.available() > 0){
+    str =  Serial.readStringUntil('\n');
+    interval = 0;
+    index = 0;
+    Serial.println("Received: " + str);
+    //Serial.read();
+    while(str[index] != '\0'){
+      interval = interval*10+str[index]-48;
+      ++index;
+    } 
+    Serial.println(interval);
+    state[0] = !state[0];
+  }
+  
+  
+    
+  if (timeNow - timeBefore > interval) {
+    timeBefore = timeNow;
+    int i;
+    for (i = 8; i >= 1; i--) {
+      state[i] = state[i-1];
+    }
+    
+    for (i = 0; i < 9; i++) {
+    digitalWrite(pin[i], state[i]);
+    }
+  }
+}
